3305403f3b15916dd38c43636d71af4bc9ab35ea
spears0703 <spears0703@gmail.com>
2023-11-26 23:34:00 +0900

    day01

--- main.js
+++ main.js
@@ -21,33 +21,73 @@ var card = {
     memorizedCnt : 0,
     words : [
         {
-            word : "단어1",
-            mean : "뜻1",
+            word : "나는 영어를 조금 할 줄 알아요.",
+            mean : "I speak a little English.",
             memorized : false
         },
         {
-            word : "단어2",
-            mean : "뜻2",
+            word : "나는 그가 무척 잘 생겼다고 생각해요.",
+            mean : "I think he's very handsome.",
             memorized : false
         },
         {
-            word : "단어3",
-            mean : "뜻3",
+            word : "나는 완두콩과 당근을 싫어해요.",
+            mean : "I hate green peas and carrots.",
             memorized : false
         },
         {
-            word : "단어4",
-            mean : "뜻4",
+            word : "우리는 히드로 공항에서 가까운 런던 서부에 살아요.",
+            mean : "we live in western London near Heathrow Airport.",
             memorized : false
         },
         {
-            word : "단어5",
-            mean : "뜻5",
+            word : "그는 지하철을 타고 출근해요.",
+            mean : "He rides the subway to go to work.",
             memorized : false
         },
         {
-            word : "단어6",
-            mean : "뜻6",
+            word : "그녀는 슈퍼마켓에 매일 아침 걸어가요.",
+            mean : "She walks to the supermarket every morning.",
+            memorized : false
+        },
+        {
+            word : "우리 아빠는 보험회사에서 일해요.",
+            mean : "My father works at an insurance company.",
+            memorized : false
+        },
+        {
+            word : "그는 프랑스어를 조금 할 줄 알아요.",
+            mean : "He spearks a little French.",
+            memorized : false
+        },
+        {
+            word : "나는 그녀가 무척 아름답다고 생각해요.",
+            mean : "I think she's very beautiful.",
+            memorized : false
+        },
+        {
+            word : "나는 치킨을 싫어해요.",
+            mean : "I hate fried chicken.",
+            memorized : false
+        },
+        {
+            word : "그들은 역에서 가까운 작은 동네에 살아요.",
+            mean : "They live in a small town near the station.",
+            memorized : false
+        },
+        {
+            word : "그녀는 버스를 타고 출근해요.",
+            mean : "She rides the bus to go to work.",
+            memorized : false
+        },
+        {
+            word : "그는 도서관에 매일 아침 걸어가요.",
+            mean : "He walks to the library every morning.",
+            memorized : false
+        },
+        {
+            word : "우리 엄마는 보험회사에서 일해요. ",
+            mean : "My mother works at an insurance company.",
             memorized : false
         }
     ],
